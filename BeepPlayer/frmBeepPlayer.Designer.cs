31c0c464fd3f307dd2dbfff407c654138f126f87
ericjywu-hue <ericjywu@gmail.com>
2026-05-07 01:31:12 +0800

    完成視覺化設計

--- BeepPlayer/frmBeepPlayer.Designer.cs
+++ BeepPlayer/frmBeepPlayer.Designer.cs
@@ -2,34 +2,21 @@
 {
     partial class frmBeepPlayer
     {
-        /// <summary>
-        /// 設計工具所需的變數。
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// 清除任何使用中的資源。
-        /// </summary>
-        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
         protected override void Dispose(bool disposing)
         {
-            if (disposing && (components != null))
-            {
-                components.Dispose();
-            }
+            if (disposing && (components != null)) components.Dispose();
             base.Dispose(disposing);
         }
 
-        #region Windows Form 設計工具產生的程式碼
+        #region Windows Form Designer generated code
 
-        /// <summary>
-        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
-        /// 這個方法的內容。
-        /// </summary>
         private void InitializeComponent()
         {
-            this.btn1 = new System.Windows.Forms.Button();
+            this.pnlVisualizer = new System.Windows.Forms.Panel();
             this.palMain = new System.Windows.Forms.Panel();
+            this.btn1 = new System.Windows.Forms.Button();
             this.btn2 = new System.Windows.Forms.Button();
             this.btn3 = new System.Windows.Forms.Button();
             this.btn4 = new System.Windows.Forms.Button();
@@ -37,123 +24,175 @@
             this.btn6 = new System.Windows.Forms.Button();
             this.btn7 = new System.Windows.Forms.Button();
             this.btn8 = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
             this.palMain.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // btn1
-            // 
-            this.btn1.Location = new System.Drawing.Point(3, 3);
-            this.btn1.Name = "btn1";
-            this.btn1.Size = new System.Drawing.Size(58, 94);
-            this.btn1.TabIndex = 0;
-            this.btn1.Text = "Do";
-            this.btn1.UseVisualStyleBackColor = true;
-            this.btn1.Click += new System.EventHandler(this.btn1_Click);
-            // 
+
+            // pnlVisualizer
+            this.pnlVisualizer.Location = new System.Drawing.Point(12, 12);
+            this.pnlVisualizer.Name = "pnlVisualizer";
+            this.pnlVisualizer.Size = new System.Drawing.Size(580, 90);
+            this.pnlVisualizer.TabIndex = 9;
+            this.pnlVisualizer.Paint += new System.Windows.Forms.PaintEventHandler(this.pnlVisualizer_Paint);
+
             // palMain
-            // 
-            this.palMain.Controls.Add(this.btn8);
-            this.palMain.Controls.Add(this.btn7);
-            this.palMain.Controls.Add(this.btn6);
-            this.palMain.Controls.Add(this.btn5);
-            this.palMain.Controls.Add(this.btn4);
-            this.palMain.Controls.Add(this.btn3);
-            this.palMain.Controls.Add(this.btn2);
+            this.palMain.BackColor = System.Drawing.Color.FromArgb(30, 30, 45);
             this.palMain.Controls.Add(this.btn1);
-            this.palMain.Font = new System.Drawing.Font("標楷體", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.palMain.Location = new System.Drawing.Point(12, 12);
+            this.palMain.Controls.Add(this.btn2);
+            this.palMain.Controls.Add(this.btn3);
+            this.palMain.Controls.Add(this.btn4);
+            this.palMain.Controls.Add(this.btn5);
+            this.palMain.Controls.Add(this.btn6);
+            this.palMain.Controls.Add(this.btn7);
+            this.palMain.Controls.Add(this.btn8);
+            this.palMain.Location = new System.Drawing.Point(12, 112);
             this.palMain.Name = "palMain";
-            this.palMain.Size = new System.Drawing.Size(520, 100);
+            this.palMain.Size = new System.Drawing.Size(580, 110);
             this.palMain.TabIndex = 8;
-            // 
-            // btn2
-            // 
-            this.btn2.Location = new System.Drawing.Point(67, 3);
+
+            // btn1 - Do
+            this.btn1.BackColor = System.Drawing.Color.FromArgb(255, 99, 99);
+            this.btn1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn1.FlatAppearance.BorderSize = 0;
+            this.btn1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+            this.btn1.ForeColor = System.Drawing.Color.White;
+            this.btn1.Location = new System.Drawing.Point(6, 5);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(68, 100);
+            this.btn1.TabIndex = 0;
+            this.btn1.Text = "Do\n(A)";
+            this.btn1.UseVisualStyleBackColor = false;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+
+            // btn2 - Re
+            this.btn2.BackColor = System.Drawing.Color.FromArgb(255, 165, 50);
+            this.btn2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn2.FlatAppearance.BorderSize = 0;
+            this.btn2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+            this.btn2.ForeColor = System.Drawing.Color.White;
+            this.btn2.Location = new System.Drawing.Point(78, 5);
             this.btn2.Name = "btn2";
-            this.btn2.Size = new System.Drawing.Size(58, 94);
+            this.btn2.Size = new System.Drawing.Size(68, 100);
             this.btn2.TabIndex = 1;
-            this.btn2.Text = "Re";
-            this.btn2.UseVisualStyleBackColor = true;
-            // 
-            // btn3
-            // 
-            this.btn3.Location = new System.Drawing.Point(131, 3);
+            this.btn2.Text = "Re\n(S)";
+            this.btn2.UseVisualStyleBackColor = false;
+
+            // btn3 - Mi
+            this.btn3.BackColor = System.Drawing.Color.FromArgb(240, 210, 50);
+            this.btn3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn3.FlatAppearance.BorderSize = 0;
+            this.btn3.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+            this.btn3.ForeColor = System.Drawing.Color.White;
+            this.btn3.Location = new System.Drawing.Point(150, 5);
             this.btn3.Name = "btn3";
-            this.btn3.Size = new System.Drawing.Size(58, 94);
+            this.btn3.Size = new System.Drawing.Size(68, 100);
             this.btn3.TabIndex = 2;
-            this.btn3.Text = "Mi";
-            this.btn3.UseVisualStyleBackColor = true;
-            // 
-            // btn4
-            // 
-            this.btn4.Location = new System.Drawing.Point(195, 3);
+            this.btn3.Text = "Mi\n(D)";
+            this.btn3.UseVisualStyleBackColor = false;
+
+            // btn4 - Fa
+            this.btn4.BackColor = System.Drawing.Color.FromArgb(100, 200, 100);
+            this.btn4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn4.FlatAppearance.BorderSize = 0;
+            this.btn4.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+            this.btn4.ForeColor = System.Drawing.Color.White;
+            this.btn4.Location = new System.Drawing.Point(222, 5);
             this.btn4.Name = "btn4";
-            this.btn4.Size = new System.Drawing.Size(58, 94);
+            this.btn4.Size = new System.Drawing.Size(68, 100);
             this.btn4.TabIndex = 3;
-            this.btn4.Text = "Fa";
-            this.btn4.UseVisualStyleBackColor = true;
-            // 
-            // btn5
-            // 
-            this.btn5.Location = new System.Drawing.Point(259, 3);
+            this.btn4.Text = "Fa\n(F)";
+            this.btn4.UseVisualStyleBackColor = false;
+
+            // btn5 - Sol
+            this.btn5.BackColor = System.Drawing.Color.FromArgb(80, 160, 240);
+            this.btn5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn5.FlatAppearance.BorderSize = 0;
+            this.btn5.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+            this.btn5.ForeColor = System.Drawing.Color.White;
+            this.btn5.Location = new System.Drawing.Point(294, 5);
             this.btn5.Name = "btn5";
-            this.btn5.Size = new System.Drawing.Size(58, 94);
+            this.btn5.Size = new System.Drawing.Size(68, 100);
             this.btn5.TabIndex = 4;
-            this.btn5.Text = "Sol";
-            this.btn5.UseVisualStyleBackColor = true;
-            // 
-            // btn6
-            // 
-            this.btn6.Location = new System.Drawing.Point(323, 3);
+            this.btn5.Text = "Sol\n(G)";
+            this.btn5.UseVisualStyleBackColor = false;
+
+            // btn6 - La
+            this.btn6.BackColor = System.Drawing.Color.FromArgb(100, 100, 240);
+            this.btn6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn6.FlatAppearance.BorderSize = 0;
+            this.btn6.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+            this.btn6.ForeColor = System.Drawing.Color.White;
+            this.btn6.Location = new System.Drawing.Point(366, 5);
             this.btn6.Name = "btn6";
-            this.btn6.Size = new System.Drawing.Size(58, 94);
+            this.btn6.Size = new System.Drawing.Size(68, 100);
             this.btn6.TabIndex = 5;
-            this.btn6.Text = "La";
-            this.btn6.UseVisualStyleBackColor = true;
-            // 
-            // btn7
-            // 
-            this.btn7.Location = new System.Drawing.Point(387, 3);
+            this.btn6.Text = "La\n(H)";
+            this.btn6.UseVisualStyleBackColor = false;
+
+            // btn7 - Si
+            this.btn7.BackColor = System.Drawing.Color.FromArgb(160, 80, 220);
+            this.btn7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn7.FlatAppearance.BorderSize = 0;
+            this.btn7.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+            this.btn7.ForeColor = System.Drawing.Color.White;
+            this.btn7.Location = new System.Drawing.Point(438, 5);
             this.btn7.Name = "btn7";
-            this.btn7.Size = new System.Drawing.Size(58, 94);
+            this.btn7.Size = new System.Drawing.Size(68, 100);
             this.btn7.TabIndex = 6;
-            this.btn7.Text = "Si";
-            this.btn7.UseVisualStyleBackColor = true;
-            // 
-            // btn8
-            // 
-            this.btn8.ForeColor = System.Drawing.Color.Red;
-            this.btn8.Location = new System.Drawing.Point(451, 3);
+            this.btn7.Text = "Si\n(J)";
+            this.btn7.UseVisualStyleBackColor = false;
+
+            // btn8 - Do'
+            this.btn8.BackColor = System.Drawing.Color.FromArgb(255, 80, 120);
+            this.btn8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn8.FlatAppearance.BorderSize = 0;
+            this.btn8.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+            this.btn8.ForeColor = System.Drawing.Color.White;
+            this.btn8.Location = new System.Drawing.Point(510, 5);
             this.btn8.Name = "btn8";
-            this.btn8.Size = new System.Drawing.Size(58, 94);
+            this.btn8.Size = new System.Drawing.Size(68, 100);
             this.btn8.TabIndex = 7;
-            this.btn8.Text = "Do";
-            this.btn8.UseVisualStyleBackColor = true;
-            // 
+            this.btn8.Text = "Do'\n(K)";
+            this.btn8.UseVisualStyleBackColor = false;
+
+            // lblStatus
+            this.lblStatus.BackColor = System.Drawing.Color.FromArgb(20, 20, 32);
+            this.lblStatus.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.lblStatus.ForeColor = System.Drawing.Color.FromArgb(160, 220, 160);
+            this.lblStatus.Location = new System.Drawing.Point(12, 232);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(580, 24);
+            this.lblStatus.TabIndex = 10;
+            this.lblStatus.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+
             // frmBeepPlayer
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(549, 136);
+            this.BackColor = System.Drawing.Color.FromArgb(22, 22, 35);
+            this.ClientSize = new System.Drawing.Size(604, 268);
+            this.Controls.Add(this.pnlVisualizer);
             this.Controls.Add(this.palMain);
+            this.Controls.Add(this.lblStatus);
+            this.MinimumSize = new System.Drawing.Size(500, 300);
             this.Name = "frmBeepPlayer";
-            this.Text = "Form1";
+            this.Text = "Beep Player";
+
             this.palMain.ResumeLayout(false);
             this.ResumeLayout(false);
-
         }
 
         #endregion
 
-        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Panel pnlVisualizer;
         private System.Windows.Forms.Panel palMain;
-        private System.Windows.Forms.Button btn8;
-        private System.Windows.Forms.Button btn7;
-        private System.Windows.Forms.Button btn6;
-        private System.Windows.Forms.Button btn5;
-        private System.Windows.Forms.Button btn4;
-        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn1;
         private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Label lblStatus;
     }
 }
-
--- BeepPlayer/frmBeepPlayer.cs
+++ BeepPlayer/frmBeepPlayer.cs
@@ -1,38 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
+using System.Drawing.Drawing2D;
 using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace BeepPlayer
 {
     public partial class frmBeepPlayer : Form
     {
+        [DllImport("kernel32.dll")]
+        public static extern bool Beep(int frequency, int duration);
+
+        private readonly int[] freq = { 523, 587, 659, 698, 784, 880, 988, 1046 };
+        private readonly string[] noteNames = { "Do", "Re", "Mi", "Fa", "Sol", "La", "Si", "Do'" };
+        private readonly Color[] noteColors =
+        {
+            Color.FromArgb(255,  99,  99),
+            Color.FromArgb(255, 165,  50),
+            Color.FromArgb(240, 210,  50),
+            Color.FromArgb(100, 200, 100),
+            Color.FromArgb( 80, 160, 240),
+            Color.FromArgb(100, 100, 240),
+            Color.FromArgb(160,  80, 220),
+            Color.FromArgb(255,  80, 120),
+        };
+
+        private System.Windows.Forms.Timer visualTimer;
+        private int visualPhase = 0;
+        private int activeNoteIndex = -1;
+
+        private int initPanelWidth, initPanelHeight;
+        private Dictionary<string, Rectangle> initControlRect = new Dictionary<string, Rectangle>();
+
         public frmBeepPlayer()
         {
             InitializeComponent();
-            InitializeButton();
-            this.Load += frmBeepPlayer_Load;
-            this.SizeChanged += frmBeepPlayer_SizeChanged;
-        }
-        [DllImport("kernel32.dll")]
-        public static extern bool Beep(int frequency, int duration);
-        int[] freq = { 523, 587, 659, 698, 784, 880, 988, 1046 };
-        private void btn1_Click(object sender, EventArgs e)
-        {
-            Button btn = sender as Button;
-            btn.Enabled = false;
-            Beep(freq[btn.TabIndex], 300);
-            btn.Enabled = true;
-        }
-        private void InitializeButton()
-        {
-            // 讓btn1~btn8共用同一個事件處理函式
+            SetupVisualTimer();
+
             btn2.Click += btn1_Click;
             btn3.Click += btn1_Click;
             btn4.Click += btn1_Click;
@@ -40,34 +45,127 @@ namespace BeepPlayer
             btn6.Click += btn1_Click;
             btn7.Click += btn1_Click;
             btn8.Click += btn1_Click;
+
+            this.Load += frmBeepPlayer_Load;
+            this.SizeChanged += frmBeepPlayer_SizeChanged;
+            this.KeyPreview = true;
+            this.KeyDown += frmBeepPlayer_KeyDown;
         }
-        int initWidth = 0;
-        int initHeight = 0;
-        Dictionary<string, Rectangle> initControl = new Dictionary<string, Rectangle>();
+
+        private void SetupVisualTimer()
+        {
+            visualTimer = new System.Windows.Forms.Timer();
+            visualTimer.Interval = 40;
+            visualTimer.Tick += (s, e) => { visualPhase++; pnlVisualizer.Invalidate(); };
+            visualTimer.Start();
+        }
+
         private void frmBeepPlayer_Load(object sender, EventArgs e)
         {
-            this.initWidth = this.palMain.Width;
-            this.initHeight = this.palMain.Height;
-            foreach (Control ctl in this.palMain.Controls)
-            {
-                this.initControl.Add(ctl.Name, new Rectangle(ctl.Left, ctl.Top,
-                ctl.Width, ctl.Height));
-            }
+            initPanelWidth = palMain.Width;
+            initPanelHeight = palMain.Height;
+            foreach (Control c in palMain.Controls)
+                initControlRect[c.Name] = new Rectangle(c.Left, c.Top, c.Width, c.Height);
+
+            lblStatus.Text = "按下琴鍵或鍵盤 [A S D F G H J K] 演奏";
         }
+
         private void frmBeepPlayer_SizeChanged(object sender, EventArgs e)
         {
-            double width = this.palMain.Width;
-            double height = this.palMain.Height;
-            double iRatioWith = width / this.initWidth;
-            double iRatioHeight = height / this.initHeight;
-            foreach (Control ctl in this.palMain.Controls)
+            if (initPanelWidth == 0) return;
+            double rW = (double)palMain.Width / initPanelWidth;
+            double rH = (double)palMain.Height / initPanelHeight;
+            foreach (Control c in palMain.Controls)
             {
-                ctl.Left = (int)(initControl[ctl.Name].Left * iRatioWith);
-                ctl.Top = (int)(initControl[ctl.Name].Top * iRatioHeight);
-                ctl.Width = (int)(initControl[ctl.Name].Width * iRatioWith);
-                ctl.Height = (int)(initControl[ctl.Name].Height *
-                iRatioHeight);
+                if (!initControlRect.ContainsKey(c.Name)) continue;
+                var r = initControlRect[c.Name];
+                c.Left = (int)(r.Left * rW);
+                c.Top = (int)(r.Top * rH);
+                c.Width = (int)(r.Width * rW);
+                c.Height = (int)(r.Height * rH);
             }
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;
+            int idx = btn.TabIndex;
+            activeNoteIndex = idx;
+
+            btn.Enabled = false;
+            Beep(freq[idx], 300);
+            btn.Enabled = true;
+
+            lblStatus.Text = $"播放音符：{noteNames[idx]}  ({freq[idx]} Hz)";
+
+            var t = new System.Windows.Forms.Timer { Interval = 400 };
+            t.Tick += (s2, e2) =>
+            {
+                activeNoteIndex = -1;
+                lblStatus.Text = "按下琴鍵或鍵盤 [A S D F G H J K] 演奏";
+                ((System.Windows.Forms.Timer)s2).Stop();
+            };
+            t.Start();
+        }
+
+        private void frmBeepPlayer_KeyDown(object sender, KeyEventArgs e)
+        {
+            string keys = "ASDFGHJK";
+            int idx = keys.IndexOf((char)e.KeyCode);
+            if (idx < 0) return;
+            e.Handled = true;
+            Button[] btns = { btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8 };
+            btns[idx].PerformClick();
+        }
+
+        private void pnlVisualizer_Paint(object sender, PaintEventArgs e)
+        {
+            var g = e.Graphics;
+            var rc = pnlVisualizer.ClientRectangle;
+            g.SmoothingMode = SmoothingMode.AntiAlias;
+
+            using (var bg = new LinearGradientBrush(rc,
+                Color.FromArgb(18, 18, 30), Color.FromArgb(30, 18, 50),
+                LinearGradientMode.Vertical))
+                g.FillRectangle(bg, rc);
+
+            int bars = 32;
+            int barW = rc.Width / bars - 2;
+            float time = visualPhase * 0.08f;
+
+            for (int i = 0; i < bars; i++)
+            {
+                float phase = (float)i / bars;
+                float amp = activeNoteIndex >= 0
+                    ? (float)(0.4 + 0.6 * Math.Abs(Math.Sin(time + phase * Math.PI * 3)))
+                    : (float)(0.05 + 0.05 * Math.Abs(Math.Sin(time * 0.3f + phase * Math.PI)));
+
+                Color barColor = activeNoteIndex >= 0
+                    ? InterpolateColor(Color.FromArgb(60, 60, 80), noteColors[activeNoteIndex], amp)
+                    : Color.FromArgb(40, 60, 90);
+
+                int h = (int)(amp * (rc.Height - 8));
+                int x = i * (barW + 2) + 2;
+                int y = rc.Height / 2 - h / 2;
+
+                using (var brush = new LinearGradientBrush(
+                    new Rectangle(x, y, barW, Math.Max(h, 1)),
+                    Color.FromArgb(200, barColor),
+                    Color.FromArgb(60, barColor),
+                    LinearGradientMode.Vertical))
+                {
+                    g.FillRectangle(brush, x, y, barW, h);
+                }
+            }
+        }
+
+        private static Color InterpolateColor(Color a, Color b, float t)
+        {
+            t = Math.Max(0f, Math.Min(1f, t));
+            return Color.FromArgb(
+                (int)(a.R + (b.R - a.R) * t),
+                (int)(a.G + (b.G - a.G) * t),
+                (int)(a.B + (b.B - a.B) * t));
         }
     }
 }
